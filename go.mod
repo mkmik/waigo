d0ebff8e99eb4ee1b40785a474cfb591ed2b5aa9
Marko Mikulicic <mmikulicic@gmail.com>
2019-12-03 12:17:19 +0100

    Use golang.org/x/tools/go/packages
    Now we also support test dependencies and default to recursive ./...
    
    Closes #1

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/integralist/go-findroot v0.0.0-20160518114804-ac90681525dc
 	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
+	golang.org/x/tools v0.0.0-20191203051722-db047d72ee39
 	gopkg.in/fsnotify.v1 v1.4.7
 )
 
--- main.go
+++ main.go
@@ -6,14 +6,26 @@ import (
 	"go/build"
 	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/golang/glog"
 	"github.com/integralist/go-findroot/find"
+	"golang.org/x/tools/go/packages"
 	fsnotify "gopkg.in/fsnotify.v1"
 )
 
-func findProjectBase(l Package) (importPath string, dir string, err error) {
+func findProjectBase() (importPath string, dir string, err error) {
+	cmd := exec.Command("go", "list", "-json")
+	b, err := cmd.Output()
+	if err != nil {
+		return "", "", err
+	}
+	var l Package
+	if err := json.Unmarshal(b, &l); err != nil {
+		return "", "", err
+	}
+
 	if l.Module != nil {
 		return l.Module.Path, l.Module.Dir, nil
 	}
@@ -29,36 +41,71 @@ func findProjectBase(l Package) (importPath string, dir string, err error) {
 	return pkg.ImportPath, dir, nil
 }
 
-func getDeps() ([]string, error) {
-	cmd := exec.Command("go", "list", "-json")
-	b, err := cmd.Output()
-	if err != nil {
-		return nil, err
+// importsUnder returns the package path of pkg pluse all the package paths
+// for all the imported packages recursively, as long as they are children
+// of the base import path.
+func importsUnder(pkg *packages.Package, base string) []string {
+	res := []string{strings.TrimSuffix(pkg.PkgPath, ".test")}
+	for _, i := range pkg.Imports {
+		if strings.HasPrefix(i.PkgPath, base) {
+			res = append(res, importsUnder(i, base)...)
+		}
 	}
-	var l Package
-	if err := json.Unmarshal(b, &l); err != nil {
+	return res
+}
+
+// Dedup sorts and deduplicates a string, returning a trimmed slice.
+func dedup(s []string) []string {
+	sort.Strings(s)
+	n := len(s)
+	if n <= 1 {
+		return s[0:n]
+	}
+	j := 1
+	for i := 1; i < n; i++ {
+		if s[i] != s[i-1] {
+			s[j] = s[i]
+			j++
+		}
+	}
+
+	return s[0:j]
+}
+
+func getDeps(pattern string) ([]string, error) {
+	baseImportPath, baseDir, err := findProjectBase()
+	if err != nil {
 		return nil, err
 	}
 
-	baseImportPath, baseDir, err := findProjectBase(l)
+	pkgs, err := packages.Load(&packages.Config{
+		Mode:  packages.NeedName | packages.NeedImports | packages.NeedDeps,
+		Tests: true,
+	}, pattern)
 	if err != nil {
 		return nil, err
 	}
+	var paths []string
+	for _, p := range pkgs {
+		paths = append(paths, importsUnder(p, baseImportPath)...)
+	}
+	paths = dedup(paths)
 
 	glog.Infof("import path: %s", baseImportPath)
 	glog.Infof("base dir: %s", baseDir)
 
 	var res []string
-	for _, d := range l.Deps {
-		if strings.HasPrefix(d, baseImportPath) && !strings.Contains(d, "/vendor/") {
+	for _, d := range paths {
+		if strings.HasPrefix(d, baseImportPath) {
 			res = append(res, filepath.Join(baseDir, strings.TrimPrefix(d, baseImportPath)))
 		}
 	}
+
 	return res, nil
 }
 
 func wait() error {
-	deps, err := getDeps()
+	deps, err := getDeps("./...")
 	if err != nil {
 		return err
 	}
@@ -92,7 +139,6 @@ func wait() error {
 		}
 	}()
 
-	deps = append(deps, ".")
 	for _, d := range deps {
 		glog.Infof("watching %q", d)
 		watcher.Add(d)
